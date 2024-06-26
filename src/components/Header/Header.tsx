de8c195d341d452f218ac4b8fc0ec0ffae4a785c
Andrey Koshelev <pippidy@gmail.com>
2024-06-26 17:38:48 +0400

    Created header menu component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,8 @@
-import { Link, NavLink } from 'react-router-dom';
 import SearchBar from '../SearchBar/SearchBar';
 // @ts-expect-error
 import logo from '../../assets/svg/logo.svg';
 import HeaderAuth from './HeaderAuth/HeaderAuth';
+import HeaderMenu from './HeaderMenu/HeaderMenu';
 
 export default function Header() {
   return (
@@ -15,53 +15,11 @@ export default function Header() {
       </a>
 
       <nav className="header__nav" role="menu">
-        <ul className="menu-main">
-          <li>
-            <NavLink
-              className="menu-main__item menu-main__item_clickable"
-              to="/"
-              title="Go to Homepage"
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <div className="menu-main__item menu-main__submenu" tabIndex={0}>
-              <span className="menu-main__submenu-name">Browse</span>
-              <div className="menu-main__submenu-holder">
-                <ul className="menu-main__submenu-list">
-                  <li className="menu-main__submenu-item">
-                    <Link to="games/genres/all#page=1" title="Browse genres">
-                      Genres
-                    </Link>
-                  </li>
-                  <li className="menu-main__submenu-item">
-                    <Link to="/characters#page=1" title="Browse characters">
-                      Characters
-                    </Link>
-                  </li>
-                  <li className="menu-main__submenu-item">
-                    <Link to="/companies#page=1" title="Browse companies">
-                      Companies
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </li>
-          <li>
-            <NavLink
-              className="menu-main__item menu-main__item_clickable"
-              to="/about"
-              title="Browse About page"
-            >
-              About
-            </NavLink>
-          </li>
-        </ul>
+        <HeaderMenu />
       </nav>
 
       <SearchBar />
+
       <HeaderAuth />
     </header>
   );
---
+++ src/components/Header/HeaderMenu/HeaderMenu.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+export default function HeaderMenu() {
+  return (
+    <ul className="menu-header">
+      <li>
+        <NavLink
+          className="menu-header__item menu-header__item_clickable"
+          to="/"
+          title="Go to Homepage"
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <div className="menu-header__item menu-header__submenu" tabIndex={0}>
+          <span className="menu-header__submenu-name">Browse</span>
+          <div className="menu-header__submenu-holder">
+            <ul className="menu-header__submenu-list">
+              <li className="menu-header__submenu-item">
+                <Link to="games/genres/all#page=1" title="Browse genres">
+                  Genres
+                </Link>
+              </li>
+              <li className="menu-header__submenu-item">
+                <Link to="/characters#page=1" title="Browse characters">
+                  Characters
+                </Link>
+              </li>
+              <li className="menu-header__submenu-item">
+                <Link to="/companies#page=1" title="Browse companies">
+                  Companies
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </li>
+      <li>
+        <NavLink
+          className="menu-header__item menu-header__item_clickable"
+          to="/about"
+          title="Browse About page"
+        >
+          About
+        </NavLink>
+      </li>
+    </ul>
+  );
+}
