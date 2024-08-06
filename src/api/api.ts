a1072d0eba844252db38c552c02261a70e088491
Andrey Koshelev <pippidy@gmail.com>
2024-08-06 11:40:24 +0400

    Prepare api.ts for Jest

--- src/api/api.ts
+++ src/api/api.ts
@@ -21,21 +21,27 @@ async function fetchAuth(): Promise<void | { access_token: string }> {
     .then((data) => data);
 }
 
-const auth = await fetchAuth();
-const configAPI: TConfigAPI = {
-  baseURL: process.env.REACT_APP_PROXY_PREFIX || '', // Proxy prefix is used for production only
-  headers: {
-    Authorization: `Bearer ${auth?.access_token}`,
-    'Client-ID': userID,
-    'Content-Type': 'application/json',
-  },
-};
+async function createConfig() {
+  const auth = await fetchAuth();
+
+  const config: TConfigAPI = {
+    baseURL: process.env.REACT_APP_PROXY_PREFIX || '', // Proxy prefix is used for production only
+    headers: {
+      Authorization: `Bearer ${auth?.access_token}`,
+      'Client-ID': userID,
+      'Content-Type': 'application/json',
+    },
+  };
+
+  return config;
+}
 
 export const getData = async ({
   apiOptions,
   signal,
 }: TGetData): Promise<TDataFull[] | undefined> => {
   const { endpoint, search, fields, filter, limit, sort, offset } = apiOptions;
+  const config = await createConfig();
 
   // Query example: "fields name,cover.image_id,videos.*,screenshots.*,aggregated_rating; limit 6; sort first_release_date desc; where aggregated_rating > 0;"
   // TODO: Refactor it
@@ -45,9 +51,9 @@ export const getData = async ({
     offset ? `offset ${offset};` : ''
   }`;
 
-  return fetch(configAPI.baseURL + `/${endpoint}`, {
+  return fetch(config.baseURL + `/${endpoint}`, {
     method: 'POST',
-    headers: configAPI.headers,
+    headers: config.headers,
     body: body,
     signal: signal,
   }).then((res) => {
@@ -61,9 +67,11 @@ export const getDataCount = async ({
   filter,
   signal,
 }: TGetDataCount) => {
-  return fetch(configAPI.baseURL + `/${endpoint}/count`, {
+  const config = await createConfig();
+
+  return fetch(config.baseURL + `/${endpoint}/count`, {
     method: 'POST',
-    headers: configAPI.headers,
+    headers: config.headers,
     body: `${filter};`,
     signal: signal,
   }).then((res) => {
@@ -75,9 +83,11 @@ export const getCategories = async (
   category: string,
   signal?: AbortSignal
 ): Promise<TCategory[] | undefined> => {
-  return fetch(configAPI.baseURL + `/${category}`, {
+  const config = await createConfig();
+
+  return fetch(config.baseURL + `/${category}`, {
     method: 'POST',
-    headers: configAPI.headers,
+    headers: config.headers,
     body: `fields name;`,
     signal: signal,
   }).then((res) => handleFetchResults(res));
