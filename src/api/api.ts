4869481cfed830d86a8f46e5d7183d47ca47bdc2
Andrey Koshelev <pippidy@gmail.com>
2024-08-09 20:14:55 +0400

    Refactored api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -11,29 +11,29 @@ import { handleFetchResults } from '../utils/utils';
 const userID = process.env.REACT_APP_IGDB_USER_ID;
 const clientSecret = process.env.REACT_APP_IGDB_CLIENT_SECRET;
 
-// Creating API config
-async function createConfig(): Promise<TConfigAPI> {
+async function fetchAuth() {
   return fetch(
     `https://id.twitch.tv/oauth2/token?client_id=${userID}&client_secret=${clientSecret}&grant_type=client_credentials`,
     {
       method: 'POST',
     }
-  )
-    .then((res) => {
-      return handleFetchResults(res);
-    })
-    .then((auth) => {
-      const config: TConfigAPI = {
-        baseURL: process.env.REACT_APP_PROXY_PREFIX || '', // Proxy prefix is used for production only
-        headers: {
-          Authorization: `Bearer ${auth?.access_token}`,
-          'Client-ID': userID ? userID : '',
-          'Content-Type': 'application/json',
-        },
-      };
+  ).then((res) => {
+    return handleFetchResults(res);
+  });
+}
 
-      return config;
-    });
+async function createConfig() {
+  const auth = await fetchAuth();
+  const config: TConfigAPI = {
+    baseURL: process.env.REACT_APP_PROXY_PREFIX || '', // Proxy prefix is used for production only
+    headers: {
+      Authorization: `Bearer ${auth?.access_token}`,
+      'Client-ID': userID ? userID : '',
+      'Content-Type': 'application/json',
+    },
+  };
+
+  return config;
 }
 
 export const getData = async ({
@@ -41,24 +41,28 @@ export const getData = async ({
   signal,
 }: TGetData): Promise<TDataFull[] | undefined> => {
   const { endpoint, search, fields, filter, limit, sort, offset } = apiOptions;
+  const config = await createConfig();
 
-  // Query example: "fields name,cover.image_id,videos.*,screenshots.*,aggregated_rating; limit 6; sort first_release_date desc; where aggregated_rating > 0;"
-  // TODO: Refactor it
-  const body = `${search ? `search "${search}";` : ''}fields ${fields}; ${
-    limit ? `limit ${limit};` : ''
-  } ${
-    sort ? `sort ${sort.property} ${sort.order ? sort.order : 'asc'};` : ''
-  } ${filter ? `where ${filter};` : ''} ${offset ? `offset ${offset};` : ''}`;
+  const querySearch = search ? `search "${search}";` : '';
+  const queryFields = `fields ${fields};`;
+  const queryLimit = limit ? `limit ${limit};` : '';
+  const querySort = sort
+    ? `sort ${sort.property} ${sort.order ? sort.order : 'asc'};`
+    : '';
+  const queryFilter = filter ? `where ${filter};` : '';
+  const queryOffset = offset ? `offset ${offset};` : '';
 
-  return createConfig().then((config) => {
-    return fetch(config.baseURL + `/${endpoint}`, {
-      method: 'POST',
-      headers: config.headers,
-      body: body,
-      signal: signal,
-    }).then((res) => {
-      return handleFetchResults(res);
-    });
+  // Body example: "fields name,cover.image_id,videos.*,screenshots.*,aggregated_rating; limit 6; sort first_release_date desc; where aggregated_rating > 0;"
+  // You have to separate each query with a ";"
+  const body = `${querySearch} ${queryFields} ${queryLimit} ${querySort} ${queryFilter} ${queryOffset}`;
+
+  return fetch(config.baseURL + `/${endpoint}`, {
+    method: 'POST',
+    headers: config.headers,
+    body: body,
+    signal: signal,
+  }).then((res) => {
+    return handleFetchResults(res);
   });
 };
 
@@ -68,26 +72,26 @@ export const getDataCount = async ({
   filter,
   signal,
 }: TGetDataCount): Promise<TDataCount | undefined> => {
-  return createConfig().then((config) => {
-    return fetch(config.baseURL + `/${endpoint}/count`, {
-      method: 'POST',
-      headers: config.headers,
-      body: `${filter};`,
-      signal: signal,
-    }).then((res) => handleFetchResults(res));
-  });
+  const config = await createConfig();
+
+  return fetch(config.baseURL + `/${endpoint}/count`, {
+    method: 'POST',
+    headers: config.headers,
+    body: `${filter};`,
+    signal: signal,
+  }).then((res) => handleFetchResults(res));
 };
 
 export const getCategories = async ({
   category,
   signal,
 }: TGetCategories): Promise<TCategory[] | undefined> => {
-  return createConfig().then((config) => {
-    return fetch(config.baseURL + `/${category}`, {
-      method: 'POST',
-      headers: config.headers,
-      body: `fields name;`,
-      signal: signal,
-    }).then((res) => handleFetchResults(res));
-  });
+  const config = await createConfig();
+
+  return fetch(config.baseURL + `/${category}`, {
+    method: 'POST',
+    headers: config.headers,
+    body: `fields name;`,
+    signal: signal,
+  }).then((res) => handleFetchResults(res));
 };
