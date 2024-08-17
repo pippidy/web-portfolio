d4d800021514f0e0578c2b9c14575b9f560e5ab6
Andrey Koshelev <pippidy@gmail.com>
2024-08-17 11:11:06 +0400

    Pagination accent color

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import { type TPaginationProps } from '../../types/pagination';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
@@ -12,13 +13,19 @@ function Pagination({
   function fillPagesArray(
     pagesArray: JSX.Element[],
     page: number,
-    currentPage: number
+    currentPage: number,
+    accent?: boolean
   ): JSX.Element[] {
+    const classLink = classNames('pagination__link', {
+      current: currentPage === page,
+      accent: accent,
+    });
+
     pagesArray.push(
       <li className="pagination__item" key={`page_${page}`}>
         <Link
           onClick={() => window.scrollTo(0, 0)}
-          className={`pagination__link ${currentPage === page && 'current'}`}
+          className={classLink}
           to={`#page=${page}`}
           title={`Go to page ${page}`}
         >
@@ -32,38 +39,40 @@ function Pagination({
 
   useEffect(() => {
     let pagesArray: JSX.Element[] = [];
-    // TODO: I probably should create an abstraction for these two
-    let elementDotsStart = (
-      <li className="pagination__item" key="page_dots_start">
-        ...
-      </li>
-    );
-    let elementDotsEnd = (
-      <li className="pagination__item" key="page_dots_end">
-        ...
-      </li>
-    );
 
-    console.log(pagesArray);
+    // Creating element with dots
+    // Pass key suffix to make it unique
+    function elementDots(keySuffix: 'start' | 'end') {
+      return (
+        <li className="pagination__item" key={`page_dots_${keySuffix}`}>
+          ...
+        </li>
+      );
+    }
 
     if (pagesAmount > 0) {
       if (currentPage < pagesLimit) {
         // First chunk of pages
         for (let i = 1; i <= pagesLimit; i++) {
           if (i > pagesAmount) break;
-          fillPagesArray(pagesArray, i, currentPage);
+          fillPagesArray(pagesArray, i, currentPage, i === pagesLimit);
         }
 
         // Adding dots at the end
-        pagesArray.push(elementDotsEnd);
+        pagesArray.push(elementDots('end'));
       } else if (currentPage >= pagesAmount - (pagesLimit - 2)) {
         // Last chunk of pages
         for (let i = pagesAmount - (pagesLimit - 1); i <= pagesAmount; i++) {
-          fillPagesArray(pagesArray, i, currentPage);
+          fillPagesArray(
+            pagesArray,
+            i,
+            currentPage,
+            i === pagesAmount - (pagesLimit - 1)
+          );
         }
 
         // Adding dots at the beginning
-        pagesArray.unshift(elementDotsStart);
+        pagesArray.unshift(elementDots('start'));
       } else {
         // Middle chunk of pages
         const halfLength = Math.floor(pagesLimit / 2);
@@ -76,12 +85,17 @@ function Pagination({
           i <= currentPage + rightSide;
           i++
         ) {
-          fillPagesArray(pagesArray, i, currentPage);
+          fillPagesArray(
+            pagesArray,
+            i,
+            currentPage,
+            i === currentPage - leftSide || i === currentPage + rightSide
+          );
         }
 
         // Adding dots at the beginning and at the end
-        pagesArray.unshift(elementDotsStart);
-        pagesArray.push(elementDotsEnd);
+        pagesArray.unshift(elementDots('start'));
+        pagesArray.push(elementDots('end'));
       }
     }
 
